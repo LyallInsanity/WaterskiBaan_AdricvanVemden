a6d88cf0c827259396955388282633fb79129a00
LyallInsanity <49395641+LyallInsanity@users.noreply.github.com>
2019-09-23 17:51:29 +0200

    First assignments

---
+++ WaterskiBaan/WaterskiBaan/IMoves.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterskiBaan
+{
+    interface IMoves
+    {
+        int Move();
+    }
+}
---
+++ WaterskiBaan/WaterskiBaan/Kabel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaterskiBaan
+{
+    class Kabel
+    {
+        private LinkedList<Lijn> _lijnen;
+
+        public Boolean IsStartPositieLeeg()
+        {
+            if (_lijnen.First == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+         }
+        public void NeemLijnInGebruik(Lijn lijn)
+        {
+            if (IsStartPositieLeeg() == true)
+            {
+                _lijnen.AddLast(lijn);
+            }
+        }
+        public void VerschuifLijnen()
+        {
+            
+        }
+        public Lijn VerwijderLijnVanKabel() { }
+        public override string ToString() { }
+    }
+}
