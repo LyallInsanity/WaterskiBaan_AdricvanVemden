a2ba6f135c2c2fe42b04ba6abbfd918cdc63ff86
LyallInsanity <49395641+LyallInsanity@users.noreply.github.com>
2019-09-23 20:02:27 +0200

    Opdracht 2

--- WaterskiBaan/WaterskiBaan/Kabel.cs
+++ WaterskiBaan/WaterskiBaan/Kabel.cs
@@ -8,31 +8,77 @@ namespace WaterskiBaan
 {
     class Kabel
     {
-        private LinkedList<Lijn> _lijnen;
 
-        public Boolean IsStartPositieLeeg()
+        private LinkedList<Lijn> _lijnen = new LinkedList<Lijn>();
+
+        public bool IsStartPositieLeeg()
         {
-            if (_lijnen.First == null)
+            if (_lijnen.Count > 0)
             {
-                return true;
+                if (_lijnen.First.Value.PositieOpDeKabel != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
             }
             else
             {
-                return false;
+                return true;
             }
-         }
+        }
         public void NeemLijnInGebruik(Lijn lijn)
         {
             if (IsStartPositieLeeg() == true)
             {
-                _lijnen.AddLast(lijn);
+                _lijnen.AddFirst(lijn);
             }
         }
         public void VerschuifLijnen()
         {
-            
+            foreach (Lijn lijn in _lijnen)
+            {
+                if (lijn.PositieOpDeKabel != 9)
+                {
+                    lijn.PositieOpDeKabel += 1;
+                }
+                else
+                {
+                    lijn.PositieOpDeKabel = 0;
+                }
+            }
         }
-        public Lijn VerwijderLijnVanKabel() { }
-        public override string ToString() { }
+        public Lijn VerwijderLijnVanKabel() {
+            if(_lijnen.Last.Value.PositieOpDeKabel == 9)
+            {
+                _lijnen.RemoveLast();
+                return _lijnen.Last.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public override string ToString()
+        {
+            string value = "";
+
+            if (_lijnen.Count > 0)
+            {
+                Lijn laatste = _lijnen.Last.Value;
+                foreach (Lijn lijn in _lijnen)
+                {
+                    value += lijn.PositieOpDeKabel.ToString();
+                    if (!lijn.Equals(laatste))
+                    {
+                        value += "|";
+                    }
+                }
+            }
+
+            return value;
+        }
     }
 }
--- WaterskiBaan/WaterskiBaan/Program.cs
+++ WaterskiBaan/WaterskiBaan/Program.cs
@@ -10,6 +10,38 @@ namespace WaterskiBaan
     {
         static void Main(string[] args)
         {
+
+            TestOpdracht2();
+        }
+        private static void TestOpdracht2()
+        {
+            Kabel kabel = new Kabel();
+            Lijn lijn1 = new Lijn();
+            Lijn lijn2 = new Lijn();
+            Lijn lijn3 = new Lijn();
+            kabel.NeemLijnInGebruik(lijn1);
+            kabel.VerschuifLijnen();
+            kabel.NeemLijnInGebruik(lijn2);
+            kabel.VerschuifLijnen();
+            kabel.VerschuifLijnen();
+            kabel.VerschuifLijnen();
+            kabel.NeemLijnInGebruik(lijn3);
+            kabel.VerschuifLijnen();
+
+
+            kabel.VerwijderLijnVanKabel();
+            Console.WriteLine(kabel);
+
+            kabel.VerschuifLijnen();
+            Console.WriteLine(kabel);
+            kabel.VerschuifLijnen();
+            kabel.VerschuifLijnen();
+            Console.WriteLine(kabel);
+            kabel.VerschuifLijnen();
+            kabel.VerschuifLijnen();
+            Console.WriteLine(kabel);
+            kabel.VerwijderLijnVanKabel();
+            Console.WriteLine(kabel);
         }
     }
 }
